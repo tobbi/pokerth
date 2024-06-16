ebbb0bc1149e5dc75fbc3860607b7adf81e43aae
Tobias Markus <tobbi.bugs@googlemail.com>
2024-06-16 16:38:21 +0200

    Replace deprecated `qStableSort` with `std::stable_sort`

--- src/gui/qt/settingsdialog/manualblindsorderdialog/manualblindsorderdialogimpl.cpp
+++ src/gui/qt/settingsdialog/manualblindsorderdialog/manualblindsorderdialogimpl.cpp
@@ -31,6 +31,7 @@
 #include "manualblindsorderdialogimpl.h"
 #include "configfile.h"
 #include "mymessagebox.h"
+#include <algorithm>
 #include <iostream>
 
 
@@ -88,7 +89,7 @@ void manualBlindsOrderDialogImpl::sortBlindsList()
 		tempIntList << listWidget_blinds->item(i)->text().toInt(&ok,10);
 	}
 
-	qStableSort(tempIntList.begin(), tempIntList.end());
+	std::stable_sort(tempIntList.begin(), tempIntList.end());
 //
 	for(i=0; i<tempIntList.count(); i++) {
 //
